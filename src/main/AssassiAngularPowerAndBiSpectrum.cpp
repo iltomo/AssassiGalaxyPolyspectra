04fd4d3b0ce720bc8026f89bbd570d2896ea59d7
iltomo <tonytroja@alice.it>
2018-03-20 14:30:46 -0300

    The actual first commit

---
+++ src/main/AssassiAngularPowerAndBiSpectrum.cpp
@@ -0,0 +1,14 @@
+/**
+ *	@file 	src/main/AssassiAngularPowerAndBiSpectrum.cpp
+ *	@date		20/03/2018
+ *	@author Atroja
+ */
+
+#include <iostream>
+
+int main (int argc, char** argv) {
+
+	std::cout << "Hello World!\n";
+
+	return 0;
+}
