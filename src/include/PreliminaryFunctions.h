f888d50ac70f7d7867e96840914ab2efb9427549
iltomo <tonytroja@alice.it>
2018-03-28 15:46:44 -0300

    Elementary and Special Functions classes added and tested

--- src/include/PreliminaryFunctions.h
+++ src/include/PreliminaryFunctions.h
@@ -22,16 +22,39 @@ class ElementaryFunctions {
 		ElementaryFunctions () = default;
 
 		double GammaC (double z);
+		void GammaC (std::complex<double> z, std::complex<double>& result);
 
 		double GammaRatioC (double z1, double z2);
+		void GammaRatioC (std::complex<double> z1, std::complex<double> z2, std::complex<double>& result);
 
 		double Hyp2F1basic (double a, double b, double c, double z);
+		void Hyp2F1basic (std::complex<double> a, std::complex<double> b, std::complex<double> c, std::complex<double> z, std::complex<double>& s);
+
+		double MinC (double a, double b);
+
+		double MaxC (double a, double b);
 
 
 		/**
 		 *	@brief	Empty destructor
 		 */
 		~ElementaryFunctions () {};
+
+};
+
+//  ----- Special Functions ----- //
+
+class SpecialFunctions {
+
+	public:
+
+		SpecialFunctions () = default;
+
+		double tminC (int l, std::complex<double> nu);
+
+		void Il (int l, std::complex<double> nu, double t, std::complex <double>& Il);
+
+		~SpecialFunctions () {};
 
 };
 
--- src/main/AssassiAngularPowerAndBiSpectrum.cpp
+++ src/main/AssassiAngularPowerAndBiSpectrum.cpp
@@ -35,8 +35,16 @@ class Assassi {
 			//test -> TestGeometry (argv [1]);
 			//test -> TestGrowthFactor (argv [1]);
 			//test -> TestLMPS (argv [1]);
-			std::shared_ptr <ElementaryFunctions> func;
-			std::cout << func -> Hyp2F1basic (1., 2., 3., 4.) << std:: endl;
+			std::shared_ptr <SpecialFunctions> func;
+			std::complex<double> nu (20., 2.);
+			std::complex<double> res;
+			func -> Il (10, nu, 1.2, res);
+			std::cout << res << std::endl;
+			/*
+			std::shared_ptr <SpecialFunctions> spec;
+			std::cout << spec -> tminC (25, nu) << std::endl;
+			spec -> Il (10, nu, 10., nu);
+			*/
 
 
 			MPI::Finalize ();
